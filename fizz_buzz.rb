58b3afd7f53566cc30fb7e7a746788286a7cca60
Sabrina Robinson <sabrinarob@gmail.com>
2018-10-29 14:18:55 -0600

    fizz_buzz based on user input

---
+++ fizz_buzz.rb
@@ -0,0 +1,15 @@
+puts "What range do you want?"
+
+input = gets.chomp.to_i
+
+(1..input).each do |num|
+  if num % 3 == 0 && num % 5 == 0
+  puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  else
+    puts num
+  end
+end
