a2edb339e0fb962f4f0ee63dcc3d706f7746dd5e
Sabrina Robinson <sabrinarob@gmail.com>
2019-03-01 12:48:36 -0700

    Implements sentence recursively

--- sentence.rb
+++ sentence.rb
@@ -1,10 +1,14 @@
 require 'pry'
 words = ["the", "clock", "struck", "twelve", "."]
 
-def sentence_structure(words)
-  sentence = []
-  binding.pry
-  # sentence_structure(words)
+def sentence_structure(words, sentence = "")
+  if words.empty?
+    return sentence
+  else
+    word = words.shift
+    updated_sentence = sentence + word + " "
+    sentence_structure(words, updated_sentence)
+  end
 
 
   # sentence = []
@@ -19,7 +23,7 @@ def sentence_structure(words)
   # words.each do |word|
   #   sentence << word
   # end
-  sentence.join(" ")
+  # sentence.join(" ")
 end
 
 p sentence_structure(words)
