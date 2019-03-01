9715fdcf5e5fa31b7bac507fe60f147111f3ba3a
Sabrina Robinson <sabrinarob@gmail.com>
2019-03-01 15:20:46 -0700

    Implements problem recursively

--- odd_even.rb
+++ odd_even.rb
@@ -1,13 +1,15 @@
 require 'pry'
 puts "Pick a number"
 number = gets.chomp.to_i
+
 def odd_or_even(number)
-  if number % 2 == 0
-    puts "You picked an even number"
+  puts "You picked an even number" if number % 2 == 0
+
+  if number % 2 == 1
+    puts "You picked an odd number"
   else
     number = number - 2
     odd_or_even(number)
-    puts "You picked an odd number"
   end
 end
 
