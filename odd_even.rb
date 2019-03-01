ee41e701a485cd79b8045169083adcfd63c869c6
Sabrina Robinson <sabrinarob@gmail.com>
2019-03-01 15:10:17 -0700

    Implements logic with modulo

--- odd_even.rb
+++ odd_even.rb
@@ -2,13 +2,10 @@ require 'pry'
 puts "Pick a number"
 number = gets.chomp.to_i
 def odd_or_even(number)
-  if  number.abs == 42
-    puts 'You picked my favorite number'
-  elsif
-    number.abs.even?
-    puts 'You picked an even number'
+  if number % 2 == 0
+    puts "You picked an even number"
   else
-    puts 'You picked an odd number'
+    puts "You picked an odd number"
   end
 end
 
