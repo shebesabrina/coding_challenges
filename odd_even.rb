473522f6526ae40152a7fd446e0626c420594903
Sabrina Robinson <sabrinarob@gmail.com>
2019-03-01 14:54:29 -0700

    Implements simplest option

---
+++ odd_even.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+def odd_or_even(number)
+  if  number.abs == 42
+    puts 'You picked my favorite number'
+  elsif
+    number.abs.even?
+    puts 'You picked an even number'
+  else
+    puts 'You picked an odd number'
+  end
+end
+
+puts odd_or_even(42)
